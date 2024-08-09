369d28961f1ad4f7dd586b908e13e2b3eff257c8
DhruvVasisht <vasisht.dhruv7@gmail.com>
2024-08-09 16:15:18 +0530

    Complete

---
+++ app.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const indexRouter=require("./routes");
+
+const http = require('http');
+const socketIO = require('socket.io');
+const server=http.createServer(app);
+const io=socketIO(server);
+
+let waitingusers=[];
+let rooms={}
+io.on("connection", function(socket) {
+    socket.on("joinroom",()=>{
+       if(waitingusers.length>0){
+         let partner=waitingusers.shift();
+         const roomname=`${socket.id}-${partner.id}`;
+         socket.join(roomname);
+         partner.join(roomname);
+         io.to(roomname).emit("joined",roomname);
+       }
+       else{
+        waitingusers.push(socket)
+       }
+    });
+
+    socket.on("signalingMessage", function(data){
+      socket.broadcast.to(data.room).emit("signalingMessage",data.message);
+
+    })
+      
+    socket.on("message",(data)=>{
+     socket.broadcast.to(data.room).emit("message",data.message)
+    })
+
+    socket.on("startVideoCall",({room})=>{
+      socket.broadcast.to(room).emit("incomingCall");
+    })
+
+    socket.on("rejectCall",({room})=>{
+      socket.broadcast.to(room).emit("callRejected");
+    })
+
+    socket.on("acceptCall", ({room})=>{
+      socket.broadcast.to(room).emit("callAccepted");
+
+    })
+
+    socket.on("disconnect", ()=>{
+      let index=waitingusers.findIndex(
+        (waitingUser) => waitingUser.id === socket.id
+      );
+      waitingusers.splice(index, 1);
+    });
+});
+
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.use('/',indexRouter);
+
+server.listen(3000,(req, res) =>{
+    console.log('Server is running on port 3000');
+});
+
+
